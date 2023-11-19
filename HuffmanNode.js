5fe1ed9e851ea0d4c557a979e9bfde255594d33d
beesan-ahmad <abumfarreh@gmail.com>
2023-11-19 15:25:56 +0200

    Create Huffman Node class to represents a node in the Huffman tree

---
+++ HuffmanNode.js
@@ -0,0 +1,11 @@
+//This class created to represents a node in the Huffman tree.
+class HuffmanNode {
+    constructor() {
+      this.frequency = 0;
+      this.character = '';
+      this.left = this.right = null;
+    }
+  }
+  
+  module.exports = HuffmanNode;
+  
