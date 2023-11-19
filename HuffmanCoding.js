e7180aea84e2b4027faddb87b6698f1797f37da7
beesan-ahmad <abumfarreh@gmail.com>
2023-11-19 15:39:27 +0200

    Create a printHuffmanCodes method

--- HuffmanCoding.js
+++ HuffmanCoding.js
@@ -2,52 +2,63 @@
 const HuffmanNode = require('./HuffmanNode');
 
 class HuffmanCoding {
-  constructor(charArray, charFreq) {
-    this.charArray = charArray;
-    this.charFreq = charFreq;
-    this.queue = [];
-    this.initializePriorityQueue();
-    this.constructHuffmanTree();
-  }
-  initializePriorityQueue() {
-    for (let i = 0; i < this.charArray.length; i++) {
-      const newNode = new HuffmanNode();
-      newNode.character = this.charArray[i];
-      newNode.frequency = this.charFreq[i];
-      newNode.left = null;
-      newNode.right = null;
-      this.queue.push(newNode);
+    constructor(charArray, charFreq) {
+        this.charArray = charArray;
+        this.charFreq = charFreq;
+        this.queue = [];
+        this.initializePriorityQueue();
+        this.constructHuffmanTree();
+        this.printHuffmanCodes();
     }
-  
-    this.queue.sort((a, b) => a.frequency - b.frequency);
-  }
+    initializePriorityQueue() {
+        for (let i = 0; i < this.charArray.length; i++) {
+            const newNode = new HuffmanNode();
+            newNode.character = this.charArray[i];
+            newNode.frequency = this.charFreq[i];
+            newNode.left = null;
+            newNode.right = null;
+            this.queue.push(newNode);
+        }
 
-  constructHuffmanTree() {
-    let root = null;
-  
-    while (this.queue.length > 1) {
-      const x = this.queue.shift();
-      const y = this.queue.shift();
-  
-      const internalNode = new HuffmanNode();
-      internalNode.frequency = x.frequency + y.frequency;
-      internalNode.character = '-';
-      internalNode.left = x;
-      internalNode.right = y;
-  
-      root = internalNode;
-  
-      this.queue.push(internalNode);
-      this.queue.sort((a, b) => a.frequency - b.frequency);
+        this.queue.sort((a, b) => a.frequency - b.frequency);
     }
-  
-    this.root = root;
-  }
-  
 
- 
+    constructHuffmanTree() {
+        let root = null;
 
-  
+        while (this.queue.length > 1) {
+            const x = this.queue.shift();
+            const y = this.queue.shift();
+
+            const internalNode = new HuffmanNode();
+            internalNode.frequency = x.frequency + y.frequency;
+            internalNode.character = '-';
+            internalNode.left = x;
+            internalNode.right = y;
+
+            root = internalNode;
+
+            this.queue.push(internalNode);
+            this.queue.sort((a, b) => a.frequency - b.frequency);
+        }
+
+        this.root = root;
+    }
+
+    printCode(root, currentCode) {
+        if (root.left === null && root.right === null && root.character.toLowerCase() !== root.character.toUpperCase()) {
+            console.log(`${root.character}: ${currentCode}`);
+            return;
+        }
+
+        this.printCode(root.left, currentCode + '0');
+        this.printCode(root.right, currentCode + '1');
+    }
+
+    printHuffmanCodes() {
+        this.printCode(this.root, '');
+    }
 }
+
 
 module.exports = HuffmanCoding;
