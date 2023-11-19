e088256a186eb8e1d8f5ed9c2cf876d98ffe7b5b
beesan-ahmad <abumfarreh@gmail.com>
2023-11-19 15:33:09 +0200

    Create PriorityQueue method

--- HuffmanCoding.js
+++ HuffmanCoding.js
@@ -6,7 +6,19 @@ class HuffmanCoding {
     this.charArray = charArray;
     this.charFreq = charFreq;
     this.queue = [];
-
+    this.initializePriorityQueue();
+  }
+  initializePriorityQueue() {
+    for (let i = 0; i < this.charArray.length; i++) {
+      const newNode = new HuffmanNode();
+      newNode.character = this.charArray[i];
+      newNode.frequency = this.charFreq[i];
+      newNode.left = null;
+      newNode.right = null;
+      this.queue.push(newNode);
+    }
+  
+    this.queue.sort((a, b) => a.frequency - b.frequency);
   }
 
  
