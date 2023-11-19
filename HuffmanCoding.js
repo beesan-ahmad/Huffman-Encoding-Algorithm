180d660d20df7fbd23cbe98ddd63a84c30b77997
beesan-ahmad <abumfarreh@gmail.com>
2023-11-19 15:30:51 +0200

    Create HuffmanCoding class

---
+++ HuffmanCoding.js
@@ -0,0 +1,17 @@
+
+const HuffmanNode = require('./HuffmanNode');
+
+class HuffmanCoding {
+  constructor(charArray, charFreq) {
+    this.charArray = charArray;
+    this.charFreq = charFreq;
+    this.queue = [];
+
+  }
+
+ 
+
+  
+}
+
+module.exports = HuffmanCoding;
